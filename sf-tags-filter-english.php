81845bb262ae257eb6dc00c8039d167d4377715f
Studiofreya <Studiofreya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 21:51:14 +0000

    trunk 1.2
    git-svn-id: https://plugins.svn.wordpress.org/sf-generate-tags/trunk@1159185 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sf-tags-filter-english.php
@@ -0,0 +1,148 @@
+<?php
+
+function filterEnglish($input_array) {
+	$output_array = array();
+	
+	$digits = array('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+		'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'nineth', 'tenth',
+		'hundred', 'hundreds', 'thousand', 'thousands');
+	
+	$addressing_words = array('another', 'both', 'dud', 'each', 'her', 'other', 'their', 'theirs');
+
+	$easy_words = array( 'after', 'against', 'ago', 'almost', 'along', 'also', 'although', 'always', 
+		'and', 'any', 'anyone', 'anytime', 'are', 'around', 'all', 'away',
+		'become', 'because', 'behind', 'before', 'between', 'but', 'com', 'common', 
+		'different', 'down', 'due', 'either', 'extra', 'ever', 'every', 
+		'far', 'false', 'for', 'forever', 'from', 'further', 'full', 
+		'here', 'hers', 'him', 'how', 'however',
+		'inn', 'into', 'its', 'itself', 'jpg', 'just', 'less', 'mean', 'more', 'most', 'myself', 
+		'next', 'never', 'new', 'nobody', 'not', 'now',
+		'often', 'once', 'only', 'org', 'other', 'others', 'our', 'ours', 'out', 'over', 'per', 'plenty', 'png', 
+		'same', 'since', 'she', 'someone', 'something', 'sure', 'such',
+		'than', 'that', 'the', 'then', 'there', 'these', 'them', 'they', 'those', 'this', 'through', 'thus', 'too', 'together', 'true', 
+		'under', 'unless', 'via',
+		'what', 'whatever', 'when', 'whenever', 'where', 'whether', 'within', 'without', 'whatever', 'which', 'while', 'whithin', 'who', 'whole', 'whose', 'why', 'with', 
+		'yes', 'yet', 'you', 'your', 'yours');
+	
+	$substantives = array('about', 'above', 'after', 'again', 'ahead', 'already', 'amount', 
+		'another', 'anything', 'approach', 'around', 'away', 
+		'carefull', 'certain', 'clear', 'correct',
+		'days', 'dead', 'define', 'different', 'difference', 'directly',
+		'easy', 'easiest', 'easily', 'end', 'even', 'everything', 'everywhere', 'final', 'good', 
+		'item', 'kinds', 'least', 'last', 'look',
+		'many', 'maybe', 'mode','much', 'needs', 'next', 'new', 'overview', 'purpose', 'reason',
+		'several', 'sites', 'site', 'some', 'something', 'step', 'stuff', 'systems', 'system', 'still',
+		'thing', 'things', 'type',
+		'very');
+		
+	$adjectives = array('actual', 'authentic', 'bad', 'best', 'better', 'closed', 'cool', 'current', 		'difficult', 
+		'easier', 'easily', 'few', 'full', 'fun', 'good', 
+		'great', 'huge', 'later', 'like', 'long', 'make', 'new', 'old', 'older', 'open', 
+		'poor', 'possible', 'proper', 'quite', 'real',
+		'seen', 'seldom', 'similar', 'small', 'taken', 'tiny', 'thrown', 'took', 'using', 'vice', 'versa');
+	
+	$verbs = array('able', 'achieve', 'add', 'afford', 'allow', 'allows', 'apply', 'arise', 'ask',
+		'been', 'beware', 'bought', 
+		'came', 'can', 'care', 'catch', 'choose', 'chosen', 'claim', 'click', 'come', 'consider', 
+		'could', 'considers', 'consists', 'contains', 'copy', 'could', 'create',
+		'decide', 'delete', 'discuss', 'did', 'does', 'doing', 'done', 'edit', 'enjoy',
+		'fill', 'find', 'found', 'gather', 'gave', 'get', 'gets', 'give', 'gone', 'got', 'guess',
+		'had', 'happen', 'has', 'have', 'helps', 'jump', 'keep', 'kept', 'let', 'like', 'look', 'loose',
+		'made', 'make', 'may', 'mean', 'means', 'might', 'must', 'own', 'paste', 'pay', 'pick', 'put', 'prefer', 'propose',	'read', 'receive', 'remember', 'run',
+		'say', 'see', 'select', 'send', 'set', 'should', 'sit', 'spend', 'start', 'stay', 'stop', 'take', 'tell', 'took', 'try', 
+		'use', 'var', 'watch', 'want', 'was', 'were', 'will', 'would');
+		
+	foreach ( $input_array as $word ) 
+	{
+		$toOutput = true;
+		
+		if(in_array( $word, $addressing_words )
+			|| in_array( $word, $easy_words )
+			|| in_array( $word, $digits )
+			|| in_array( $word, $substantives )
+			|| in_array( $word, $adjectives )
+			|| in_array( $word, $verbs )
+			|| wordEndsWith($word, "al")
+			|| wordEndsWith($word, "able")
+			|| wordEndsWith($word, "ly")
+			|| wordEndsWith($word, "ed")
+			|| wordEndsWith($word, "'s")
+			|| wordEndsWith($word, "es")
+			|| wordEndsWith($word, "ing")
+			|| wordEndsWith($word, "est")
+			|| wordEndsWith($word, "ght")
+			|| wordEndsWith($word, "ugh")
+		)
+		{ 	
+			$toOutput = false;
+		} 
+
+		if($toOutput == TRUE){
+			array_push($output_array, $word);
+		}
+	}
+	
+	return $output_array;
+}
+
+function limitTagsTo($tagsArrayFilteredEnglish, $limitNumber) {
+	$output_array = array();
+	
+	$tagsArrayFilteredEnglish = excludeSecondImportantWords($tagsArrayFilteredEnglish);
+	
+	if($tagsArrayFilteredEnglish > $limitNumber) {
+		$i = 1;
+		//filter by letter number
+		foreach($tagsArrayFilteredEnglish as $word) {
+		
+			if($i > $limitNumber) {
+				break;
+			}
+			
+			$tagLength = strlen($word);
+			if($tagLength <= 7 && $tagLength > 3) {
+				array_push($output_array, $word);
+				$i++;
+			}			
+		}
+	}
+	
+	return $output_array;
+}
+
+function excludeSecondImportantWords($input_array) {
+	$output_array = array();
+	
+	$substantives = array('advice', 'chance', 'model', 'failure', 'passion', 'time', 'value');
+	
+	$easy_words = array('back', 'instead', 'lot');
+	
+	$adjectives = array('big', 'careful', 'free', 'hard', 'kind', 'major', 'stuck', 'wise', 'young');
+	
+	$verbs = array('control', 'match', 'track',  'update', 'upgrade');
+	
+	foreach ( $input_array as $word ) 
+	{
+		$toOutput = true;
+		
+		if(in_array( $word, $easy_words )
+			|| in_array( $word, $substantives )
+			|| in_array( $word, $adjectives )
+			|| in_array( $word, $verbs )
+			|| wordEndsWith($word, "s")
+			|| wordEndsWith($word, "en")
+			|| wordEndsWith($word, "y")
+			|| wordEndsWith($word, "t")
+		)
+		{ 	
+			$toOutput = false;
+		} 
+
+		if($toOutput == TRUE){
+			array_push($output_array, $word);
+		}
+	}
+	
+	return $output_array;
+}
+?>
---
+++ sf-tags.php
@@ -0,0 +1,158 @@
+<?php 
+/**
+ * Plugin Name: SF Generate Tags
+ * Plugin URI: http://studiofreya.com
+ * Description: This plugin autogenerates tags for your posts from attached images names and post text
+ * Version: 1.2
+ * Author: Studiofreya AS
+ * Author URI: http://studiofreya.com
+ * License: GPL3
+ */
+
+function init_tags() {
+	
+	if ( is_admin() ) {
+		require( 'sf-tags-admin.php' );
+		require( 'sf-tags-filter-english.php' );
+		new SFTags_Admin();
+	}
+	
+	load_textdomain( 'sf-tags', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
+}
+add_action( 'plugins_loaded', 'init_tags' );
+
+
+function sf_tags_load_jquery() {
+    wp_enqueue_script( 'jquery' );
+}
+add_action( 'wp_enqueue_script', 'sf_tags_load_jquery' );
+
+
+function sf_ajax_tags_scripts() { 
+?>
+	<script type="text/javascript">
+    jQuery(document).ready(function()
+	{
+		jQuery('#btnSubmit').click(function ()
+		{
+			var sf_post_id = jQuery('#sf_post_id').val();
+			var sf_remove_words = jQuery('#sf_remove_words').val();
+			var sf_limit_tags_to = jQuery('#sf_limit_tags').val();
+			var sf_from_images = jQuery('#sf_from_images').is(':checked')? 1:0;
+			var sf_from_text = jQuery('#sf_from_text').is(':checked')? 1:0;
+			
+			jQuery.ajax({
+				type: "POST",
+				url: ajaxurl,
+				
+				data: {
+					action : 'sf_generate_tags', 
+					post_id: sf_post_id,
+					remove_words: sf_remove_words,
+					limit_tags: sf_limit_tags_to,
+					from_images : sf_from_images,
+					from_text : sf_from_text
+				}
+				}).done(function( msg ) {
+					jQuery("#new-tag-post_tag").val(msg);
+					jQuery(".ajaxtag input.button").trigger('click');   
+				 });
+		});
+	});
+</script>
+<?php }
+add_action('admin_footer','sf_ajax_tags_scripts');
+
+function sf_generate_tags() {
+	$postId = (int)$_POST['post_id'];
+	
+	//get existing tags
+	$tags = wp_set_post_tags($postId);
+
+	$s = "";
+	
+	$tagsFromImages = (int)$_POST['from_images'];
+	if($tagsFromImages == 1)
+	{
+		$media = get_attached_media( 'image', $postId );
+		foreach($media as $attachment_meta)
+		{
+			$s .= $attachment_meta->post_name;
+			$s .= "-";
+		}
+	}
+	
+	$tagsFromText = (int)$_POST['from_text'];
+	if($tagsFromText == 1)
+	{
+		$content_post = get_post($postId);
+		$content = $content_post->post_content;
+		$s .= $content;
+	}
+
+	//split string and make an array of words
+	$wordsArray = preg_split("/[\s,-.]+/", $s);
+	$tagsArray = array_unique(array_map('strtolower', $wordsArray));
+	sort($tagsArray);
+		
+	//exclude user defined words
+	$removeWords_string = $_POST['remove_words'];
+	$removables = preg_split("/[\s,-.]+/", $removeWords_string);
+	$tagsArrayExcludedUser = array_diff($tagsArray, $removables);
+	
+	//exclude words with some literals
+	$tagsArrayExcludedLiterals = removeWordsWithLiterals($tagsArrayExcludedUser);
+	$tagsArrayFilteredEnglish = filterEnglish($tagsArrayExcludedLiterals);
+	
+	//limit number of tags
+	$limitNumber = $_POST['limit_tags'];
+	if(isset($limitNumber) && $limitNumber > 0 && count($tagsArrayFilteredEnglish) > $limitNumber) {
+		$tagsArrayFilteredEnglish = limitTagsTo($tagsArrayFilteredEnglish, $limitNumber);
+	}
+	
+	//make tags a comma separated array
+	$commaSeparatedTags = implode(",", $tagsArrayFilteredEnglish);
+	array_push($tags, $commaSeparatedTags);
+	
+	//send response
+	$response = implode(",", $tags);
+	echo "$response";
+	
+	wp_die();
+}
+add_action("wp_ajax_sf_generate_tags", "sf_generate_tags");
+
+function removeWordsWithLiterals($input_array)
+{
+	$output_array = array();
+	
+	$letters_only="/^\pL+$/u";
+	foreach ( $input_array as $word ) 
+	{
+		$toOutput = true;
+		if(preg_match( $letters_only, $word ))
+		{
+			//letters only -> ok
+		} else {
+			$toOutput = false;
+		}
+		
+		if(strlen($word) <= 2)
+		{ 	
+			$toOutput = false;
+		} 
+
+		if($toOutput == TRUE){
+			array_push($output_array, $word);
+		}
+	}
+	
+	return $output_array;
+}
+
+function wordEndsWith($haystack, $needle) {
+    // search forward starting from end minus needle length characters
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+}
+
+?>
