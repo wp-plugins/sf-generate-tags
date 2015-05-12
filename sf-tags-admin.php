58dd594cff6e6bf3b5ef91dafaaa497e63b58758
Studiofreya <Studiofreya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 21:42:07 +0000

    Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/sf-generate-tags/tags/1.2@1159167 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sf-tags-admin.php
@@ -0,0 +1,35 @@
+<?php
+class SFTags_Admin {
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		// Box for advanced tags
+		add_action( 'add_meta_boxes', array(__CLASS__, 'add_meta_boxes'), 10, 1 );
+	}
+	
+	public static function add_meta_boxes( $post_type ) {
+		add_meta_box('sf-tags-settings', __('Autogenerate Tags - Settings', 'sf-tags'), array(__CLASS__, 'metabox'), $post_type, 'side', 'core' );
+	}
+	
+	public static function metabox( $post ) {
+?>	
+	<div class="sf-generate-tags-form">
+		<input type="checkbox" name="sf_from_images" value="sf_from_images" id="sf_from_images" checked="checked" /><?php _e('From Images', 'sf-tags'); ?>
+		<input type="checkbox" name="sf_from_text" value="sf_from_images" id="sf_from_text" checked="checked"/><?php _e('From Text', 'sf-tags') ?>
+		<input type="hidden" name="sf_post_id" id="sf_post_id" value="<?php global $post; echo "$post->ID"; ?>">
+		<p><?php _e('Limit tags to:', 'sf-tags') ?></p>
+		<input type="number" name="sf_limit_tags" id="sf_limit_tags" value="40"/>
+		<br />
+    	<a class="button" id="btnSubmit"><?php _e('Generate Tags', 'sf-tags') ?></a>
+		<br />
+		<p><?php _e('Exclude words (comma separated):', 'sf-tags') ?></p>
+		<input type="text" name="sf_remove_words" id="sf_remove_words" value=""/>
+		<br />
+		<br />
+		<?php _e('(save the post to get all of the content evaluated)', 'sf-tags') ?>
+	</div>
+
+<?php
+	}
+}
